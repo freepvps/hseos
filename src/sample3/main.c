c3bf041e1f4f88462d38a9b00e6c651a3766842a
suns <suns@hdev>
2019-12-02 11:46:25 +0100

    rm bins, add sample

---
+++ src/sample3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <memory.h>
+#include <stdint.h>
+
+
+void magic(int x[4]) {
+    printf("%d %d %d %d\n", x[0], x[1], x[2], x[3]);
+}
+
+int main() {
+    int x[4] = {1, 2};
+    magic(x);
+    int y[2][2] = {{1, 2}, {3, 4}};
+    magic(y);  // just warn
+    
+    return 0;
+}
