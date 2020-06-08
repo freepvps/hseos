1356d580535aef29608b3aed2af59ceaa8cf9d31
suns <freepvps@gmail.com>
2020-06-08 14:14:49 +0200

    lol, fixed

--- src/atomic/list/main.c
+++ src/atomic/list/main.c
@@ -15,6 +15,7 @@ typedef struct AtomicList atomic_list_t;
 void atomic_push(atomic_list_t* head, atomic_list_t* node) {
     while (1) {
         atomic_list_t* current_next = __atomic_load_n(&head->next, __ATOMIC_SEQ_CST);
+        node->next = current_next;
         if (__atomic_compare_exchange(&head->next, &current_next, &node, 1, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
             break;
         }
